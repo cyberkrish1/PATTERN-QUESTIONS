a280b005f91894ade35884ecaec4f10d3a4907ee
cyberkrish1 <macbook131213@gmail.com>
2024-08-22 16:08:10 +0530

    Added 4th Pattern

---
+++ 04_num.cpp
@@ -0,0 +1,22 @@
+/*
+Descending Pattern
+54321
+54321
+54321
+54321
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row,i,j;
+    cout<<"Enter Number Of Rows:";
+    cin>>row;
+    for(i=0;i<row;i++){
+        for(j=row;j>0;j--){
+            cout<<j;
+        }        
+        cout<<endl;
+    }
+    return 0;
+}
