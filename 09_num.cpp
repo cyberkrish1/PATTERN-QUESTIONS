23e816b0115164c15765472a0c4bdd39dfebf65b
cyberkrish1 <macbook131213@gmail.com>
2024-08-22 21:35:49 +0530

    Added Solution

--- 09_num.cpp
+++ 09_num.cpp
@@ -6,6 +6,37 @@
 12345
 */
 
+// And
+/*
+1
+22
+333
+4444
+55555
+*/
+
+//1st part
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int row,i,j;
+//     cout<<"Enter Number Of Rows:";
+//     cin>>row;
+//     for(i=0;i<row;i++){
+       
+//         for(j=0;j<=i;j++){
+//             cout<<j+1;
+
+//         }        
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+//2nd part
+
 #include<iostream>
 using namespace std;
 int main()
@@ -16,10 +47,11 @@ int main()
     for(i=0;i<row;i++){
        
         for(j=0;j<=i;j++){
-            cout<<j+1;
+            cout<<i+1;
 
         }        
         cout<<endl;
     }
     return 0;
 }
+
