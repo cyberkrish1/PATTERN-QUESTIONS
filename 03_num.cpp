c59f10694265dc9bc58d6c6ae8d3a98eb54d0ca6
cyberkrish1 <macbook131213@gmail.com>
2024-08-22 15:57:56 +0530

    Added Third Pattern

---
+++ 03_num.cpp
@@ -0,0 +1,22 @@
+/*
+12345
+12345
+12345
+12345
+12345
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row,i,j;
+    cout<<"Enter Number Of Rows:";
+    cin>>row;
+    for(i=0;i<row;i++){
+        for(j=0;j<row;j++){
+            cout<<j+1;
+        }        
+        cout<<endl;
+    }
+    return 0;
+}
