8bee22fe09072d071e647587fc6ebd538783ecf0
cyberkrish1 <macbook131213@gmail.com>
2024-08-22 23:25:46 +0530

    Added Solution

--- 01_star_ and_num.cpp
+++ 01_star_ and_num.cpp
@@ -26,47 +26,54 @@
  4444
 55555 */
 
+/*
+    1
+   12
+  123
+ 1234
+12345
+ */
 //1st Part
 
-// #include<iostream>
+#include<iostream>
 
-// using namespace std;
-// int main()
-// {
-//     int rows,i,j,k;
-//     cout<<"Enter no.of rows:";
-//     cin>>rows;
-//     for(i=0;i<rows;i++){
-//             for(k=0;k<=rows-i-1;k++){
-//                 cout<<" ";
-//             }
-//             for(j=0;j<=i;j++){
-//             cout<<"*";
-//         }
-//         cout<<"\n";
-//     }
-//     return 0;
-// }
+using namespace std;
+int main()
+{
+    int rows,i,j,k;
+    cout<<"Enter no.of rows:";
+    cin>>rows;
+    for(i=0;i<rows;i++){
+            for(k=0;k<rows-i-1;k++){
+                cout<<" ";
+            }
+            for(j=0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
 
 //2nd Part
 
-// #include<iostream>
+#include<iostream>
 
-// using namespace std;
-// int main()
-// {
-//     int rows,i,j,k;
-//     cout<<"Enter no.of rows:";
-//     cin>>rows;
-//     for(i=0;i<rows;i++){
-//             for(k=rows;k>i;k--){
-//                 cout<<"*";
-//             }
+using namespace std;
+int main()
+{
+    int rows,i,j,k;
+    cout<<"Enter no.of rows:";
+    cin>>rows;
+    for(i=0;i<rows;i++){
+            for(k=rows;k>i;k--){
+                cout<<"*";
+            }
         
-//         cout<<"\n";
-//     }
-//     return 0;
-// }
+        cout<<"\n";
+    }
+    return 0;
+}
 
 //3rd 
 
@@ -83,11 +90,30 @@ int main()
             for(k=0;k<=rows-i-1;k++){
                 cout<<" ";
             }
-            for(j=0;j<=i;j++){
-            cout<<i+1;
+            for(j=0;j<i;j++){
+            cout<<j+1;
         }
         cout<<"\n";
     }
     return 0;
 }
+//4th
+#include<iostream>
 
+using namespace std;
+int main()
+{
+    int rows,i,j,k;
+    cout<<"Enter no.of rows:";
+    cin>>rows;
+    for(i=0;i<rows;i++){
+            for(k=0;k<rows-i-1;k++){
+                cout<<" ";
+            }
+            for(j=0;j<=i;j++){
+            cout<<j+1;
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
