4d4518716105c09f94de094db11cac0bee9b10c8
cyberkrish1 <macbook131213@gmail.com>
2024-08-22 15:42:33 +0530

    Added Solution

---
+++ 01_star.cpp
@@ -0,0 +1,27 @@
+/*
+    *
+   **
+  ***
+ ****
+*****
+
+ */
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int rows,i,j,k;
+    cout<<"Enter no.of rows:";
+    cin>>rows;
+    for(i=0;i<rows;i++){
+            for(k=0;k<=rows-i-1;k++){
+                cout<<" ";
+            }
+            for(j=0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
